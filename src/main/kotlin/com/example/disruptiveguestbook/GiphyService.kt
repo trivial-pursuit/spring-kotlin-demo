5a3bc197f458aded3adf35c91b2680465ddd0de1
Tobias Bayer <tobayer@googlemail.com>
2019-07-13 13:06:34 +0200

    Replace RestTemplate with Micronaut HTTP client

--- src/main/kotlin/com/example/disruptiveguestbook/GiphyService.kt
+++ src/main/kotlin/com/example/disruptiveguestbook/GiphyService.kt
@@ -1,25 +1,35 @@
 package com.example.disruptiveguestbook
 
 import com.fasterxml.jackson.annotation.JsonProperty
-
+import io.micronaut.http.HttpRequest
+import io.micronaut.http.client.RxHttpClient
+import io.micronaut.http.uri.UriTemplate
 import javax.inject.Singleton
 
 @Singleton
 class GiphyService(
-    templateBuilder: RestTemplateBuilder,
+    private val httpClient: RxHttpClient,
     private val giphyProperties: GiphyProperties
 ) {
 
-    val restTemplate = templateBuilder.rootUri(giphyProperties.url).build()
-
     fun findUrlForTag(tag: String): String? {
 
-        val giphyResponse = restTemplate.getForEntity<GiphyResponse>(
-            "/search?api_key={api-key}&q={tag}&limit={limit}&offset=0&rating=G&lang=en",
-            giphyProperties.apikey, tag, 10
-        )
+        val uri = UriTemplate.of("/search?api_key={api-key}&q={tag}&limit={limit}&offset=0&rating=G&lang=en")
+                .expand(mapOf(
+                        "api-key" to giphyProperties.apikey,
+                        "tag" to tag,
+                        "limit" to 10))
 
-        return giphyResponse.body?.data?.shuffled()?.firstOrNull()?.embedUrl
+        return httpClient
+                .retrieve(
+                        HttpRequest.GET<Any>(uri),
+                        GiphyResponse::class.java)
+                .firstElement()
+                ?.blockingGet()
+                ?.data
+                ?.shuffled()
+                ?.firstOrNull()
+                ?.embedUrl
     }
 }
 
