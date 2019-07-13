4ee2894c1504f86336a31425f048164d06fe1a94
Tobias Bayer <tobayer@googlemail.com>
2019-07-13 12:47:53 +0200

    Replace Spring Data Mongo with Mongo client

--- src/main/kotlin/com/example/disruptiveguestbook/Message.kt
+++ src/main/kotlin/com/example/disruptiveguestbook/Message.kt
@@ -1,13 +1,9 @@
 package com.example.disruptiveguestbook
 
-import org.springframework.data.annotation.Id
-import org.springframework.data.mongodb.core.mapping.Document
 import java.time.Instant
-import java.util.UUID
+import java.util.*
 
-@Document
 data class Message(
-    @Id
     val id: UUID = UUID.randomUUID(),
     val createdAt: Instant = Instant.now(),
     val fromUser: String? = null,
--- src/main/kotlin/com/example/disruptiveguestbook/MessageRepository.kt
+++ src/main/kotlin/com/example/disruptiveguestbook/MessageRepository.kt
@@ -1,8 +1,43 @@
 package com.example.disruptiveguestbook
 
-import org.springframework.data.mongodb.repository.MongoRepository
-import java.util.UUID
+import com.mongodb.client.model.Filters.eq
+import com.mongodb.reactivestreams.client.MongoClient
+import com.mongodb.reactivestreams.client.MongoCollection
+import io.reactivex.Flowable
+import io.reactivex.Single
+import javax.inject.Inject
 
-interface MessageRepository : MongoRepository<Message,UUID>{
-    fun findByFromUser(user: String):List<Message>
+
+
+class MessageRepository(
+        @Inject private val mongoClient: MongoClient
+) {
+
+    fun findAll(): List<Message> {
+        return Flowable
+                .fromPublisher(
+                        getCollection()
+                                .find()
+                ).toList().blockingGet()
+    }
+
+    fun findByFromUser(user: String): List<Message> {
+        return Flowable
+                .fromPublisher(
+                        getCollection()
+                        .find(eq("fromUser", user))
+        ).toList().blockingGet()
+    }
+
+    fun save(message: Message): Message{
+        return Single
+                .fromPublisher(getCollection()
+                        .insertOne(message)).map { message }.blockingGet()
+    }
+
+    private fun getCollection(): MongoCollection<Message> {
+        return mongoClient
+                .getDatabase("test")
+                .getCollection("message", Message::class.java)
+    }
 }
