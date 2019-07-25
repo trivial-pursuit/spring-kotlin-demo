547082c221a39ab67e59c54bcbaccd960fbc052b
Tobias Bayer <tobayer@googlemail.com>
2019-07-25 15:48:13 +0200

    Add format

--- src/main/kotlin/com/example/disruptiveguestbook/Message.kt
+++ src/main/kotlin/com/example/disruptiveguestbook/Message.kt
@@ -3,12 +3,13 @@ package com.example.disruptiveguestbook
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable
+import com.fasterxml.jackson.annotation.JsonFormat
 import java.util.*
 
 @DynamoDBTable(tableName = "guestbook")
 data class Message(
         @DynamoDBHashKey var id: String? = null,
-        @DynamoDBAttribute var createdAt: Date? = null,
+        @DynamoDBAttribute @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ssXXX") var createdAt: Date? = null,
         @DynamoDBAttribute var fromUser: String? = null,
         @DynamoDBAttribute var text: String? = null,
         @DynamoDBAttribute var giphyLink: String? = null
